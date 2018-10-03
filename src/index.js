f5eae2b13a4b174abdeaf5e09be4e3e1a89b893f
bjjeong <bjjeong@umich.edu>
2018-10-03 15:45:58 -0700

    Initial commit for Factr coding exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import configureStore from './store/store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  let store = configureStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+  window.dispatch = store.dispatch;
+  window.getState = store.dispatch;
+
+  ReactDOM.render( <App store = { store } />, root);
+});
+
 serviceWorker.unregister();
