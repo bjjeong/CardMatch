a07c090a6ceb4cbd2636e63b0c125d9cba4a7b1f
bjjeong <bjjeong@umich.edu>
2018-10-03 16:53:43 -0700

    Start on the board

---
+++ src/components/board/board.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Board = props => {
+  return(
+    <div className="board-container">
+      <div className="playing-area">
+      hello
+      </div>
+    </div>
+  );
+};
+
+export default Board;
--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Board from '../board/board_container';
 
 const Homepage = props => {
 
@@ -7,20 +8,22 @@ const Homepage = props => {
       <nav>
         <div className="nav-wrapper">
           <a href="/" className="brand-logo">
-            <div className="logo">
-
-            </div>
+            <div className="logo"></div>
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="/">Info</a></li>
-            <li><a href="https://www.github.com/bjjeong" target="_blank">Github</a></li>
-            <li><a href="https://www.linkedin.com/in/bjjeong" target="_blank">LinkedIn</a></li>
+            <li><a 
+              href="https://www.github.com/bjjeong"
+              rel="noopener noreferrer"
+              target="_blank">Github</a></li>
+            <li><a
+              href="https://www.linkedin.com/in/bjjeong"
+              rel="noopener noreferrer"
+              target="_blank">LinkedIn</a></li>
           </ul>
         </div>
       </nav>
-      <div>
-        hello
-      </div>
+      <Board />
     </div>
   );
 };
