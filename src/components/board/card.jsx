b402ea1d11dc1bda1d931686f582a42813f5b87e
bjjeong <bjjeong@umich.edu>
2018-10-03 18:45:28 -0700

    Be able to change color of cards on click

--- src/components/board/card.jsx
+++ src/components/board/card.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Card = props => {
-  let { card } = props;
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    }
 
-  return(
-    <li className="card">
-      {card.value}
-    </li>
-  )
+    this.flipCard = this.flipCard.bind(this);
+  }
+
+  flipCard(id) {
+    this.setState({clicked: !this.state.clicked})
+  }
+
+  render() {
+    let { card } = this.props;
+
+    return (
+      <li className={"card"} onClick={() => this.flipCard(card.id)}>
+        <div className={`card-value ${this.state.clicked ? 'up' : ''}`}>
+          {card.value}
+        </div>
+      </li>
+    )
+  }
 }
 
 export default Card;
