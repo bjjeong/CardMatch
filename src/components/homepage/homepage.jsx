70632d288eb6b6df81d81ea62667a0104230cf8a
bjjeong <bjjeong@umich.edu>
2018-10-08 13:19:51 -0700

    Create hamburger menu on mobile

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -10,8 +10,11 @@ const Homepage = props => {
           <a href="/" className="left">
             <div className="logo"></div>
           </a>
-          <ul id="nav-mobile" className="right">
-            <li><a class="modal-trigger" href="#modal1">How To Play</a></li>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger right">
+            <i className="fas fa-bars"></i>
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a className="modal-trigger" href="#modal1">How To Play</a></li>
             <li><a 
               href="https://www.github.com/bjjeong"
               rel="noopener noreferrer"
@@ -23,9 +26,20 @@ const Homepage = props => {
           </ul>
         </div>
       </nav>
+      <ul className="sidenav" id="mobile-demo">
+        <li><a className="modal-trigger" href="#modal1">How To Play</a></li>
+        <li><a
+          href="https://www.github.com/bjjeong"
+          rel="noopener noreferrer"
+          target="_blank">Github</a></li>
+        <li><a
+          href="https://www.linkedin.com/in/bjjeong"
+          rel="noopener noreferrer"
+          target="_blank">LinkedIn</a></li>
+      </ul>
       <Board />
-      <div id="modal1" class="modal">
-        <div class="modal-content">
+      <div id="modal1" className="modal">
+        <div className="modal-content">
           <h4><strong>How to Play!</strong></h4>
           <p>
             This is a simple card-matching game built with React and Redux, created by me as a coding exercise for Factr.
